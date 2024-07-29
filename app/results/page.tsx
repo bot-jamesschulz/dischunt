d096351c29287bae5f8e1b0479991a364d85d01e
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2024-07-29 14:53:37 -0700

    Added suspense for searchParam usage components

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,58 +1,14 @@
-"use client"
-
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState, Suspense } from "react";
-import { InputForm } from "@/components/ui/input-form";
-import Listing from "@/components/listing";
-import Supabase from "@/db/config";
-import { type DiscSearch } from '@/db/types';
-
+import Listings from '@/components/listings';
+import { InputForm } from '@/components/ui/input-form';
+import { Suspense } from "react";
 
 export default function Results() {
-    const [listings, setListings] = useState<DiscSearch>([]);
-    const [loadingState, setLoadingState] = useState<undefined | 'loading' | 'loaded' | 'no results'>();
-    const searchParams = useSearchParams();
-
-    useEffect(() => {
-        
-        const fetchListings = async () => {
-            
-            
-            if (!Supabase) return;
-
-            const query = searchParams.get('query') || "";
-            console.log('query', query)
-            setLoadingState('loading')
-
-            const { data, error } = await Supabase.rpc("disc_search", {
-                disc: query
-            })
-
-            if (error) {
-                console.error("Error fetching listings", error)
-                return
-            }
-
-            setLoadingState("loaded");
-
-            if (!data?.length) {
-                setLoadingState("no results")
-            } else {
-                setListings(data)
-            }
-        }
-
-        fetchListings();
-    }, [searchParams]);
-
 
     return (
         <Suspense>
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
                 <InputForm/>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 border p-10">
-                    {listings.map((listingData) => <Listing key={listingData.details_url} discListing={listingData} />)}
-                </div>
+                <Listings />
             </main>
         </Suspense>
     );
