9565478ee20da6d87634eb572ead7010bc497fae
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2024-08-03 01:28:23 -0700

    Temporary removal of filterTags

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,10 @@ const nextConfig = {
             hostname: "**",
           },
         ],
-      }
+      },
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
 };
 
 export default nextConfig;
