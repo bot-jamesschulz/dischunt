970bdc283c63f185816e4fcc04cc857bac01061f
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2024-08-21 22:34:49 -0700

    Updated pricing calculation to reflect database changes in no longe rtsoring decimal points

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Disc List",
+  title: "Disc Search",
   description: "Find your next disc",
 };
 
